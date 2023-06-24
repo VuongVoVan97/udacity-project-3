d9758f8b1b48d1bb858a3d44c1d649468ee69c67
VuongVoVan97 <vuong011197@gmail.com>
2023-06-24 14:39:18 +0700

    commot

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    "http://af35b68e5ca38499ea56fc1b853e0b7b-1095243039.us-east-1.elb.amazonaws.com:8080/api/v0",
 };
 
 /*
